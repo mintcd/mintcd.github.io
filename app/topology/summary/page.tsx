54b83d42d54183963d1da424ae7935c716045d19
Đăng Minh <dangminh2208@gmail.com>
2024-01-26 01:14:01 +0700

    Buildable

--- app/topology/summary/page.tsx
+++ app/topology/summary/page.tsx
@@ -26,41 +26,41 @@ export default function Graph() {
 }
 
 
-const Block = ({ id, item, includeId, includeName }:
-    { id: string, item: Statement, includeId: boolean, includeName: boolean }) => {
-    const typeClassNames = {
-        theorem: 'bg-[#C3E3E7]',
-        definition: 'bg-[#c4e0b0]',
-        lemma: 'bg-[#FFD9B7]'
-    };
+// const Block = ({ id, item, includeId, includeName }:
+//     { id: string, item: Statement, includeId: boolean, includeName: boolean }) => {
+//     const typeClassNames = {
+//         theorem: 'bg-[#C3E3E7]',
+//         definition: 'bg-[#c4e0b0]',
+//         lemma: 'bg-[#FFD9B7]'
+//     };
 
-    function handleClick() {
-        console.log(`Proof of`, id);
-    }
+//     function handleClick() {
+//         console.log(`Proof of`, id);
+//     }
 
-    const typeClassName = (typeClassNames as { [key: string]: string })[item.type] || '';
+//     const typeClassName = (typeClassNames as { [key: string]: string })[item.type] || '';
 
-    const content = `${includeId ? `$$\\textbf{${item.type.charAt(0).toUpperCase() + item.type.slice(1)} ${id}}$$` : ""} 
-                    ${includeName ? item.name ? `$\\textbf{${item.name}.}$` : "" : ""} ${item.short || item.content}`;
+//     const content = `${includeId ? `$$\\textbf{${item.type.charAt(0).toUpperCase() + item.type.slice(1)} ${id}}$$` : ""}
+//                     ${includeName ? item.name ? `$\\textbf{${item.name}.}$` : "" : ""} ${item.short || item.content}`;
 
-    return (
-        <div
-            id={id}
-            className={`border-grey border-solid border-2 rounded-[20px] p-5 my-5 mx-[20px] hover:opacity-50 ${typeClassName}`}
-            onClick={handleClick}
-        >
-            <Node content={content} />
-        </div>
-    );
-};
+//     return (
+//         <div
+//             id={id}
+//             className={`border-grey border-solid border-2 rounded-[20px] p-5 my-5 mx-[20px] hover:opacity-50 ${typeClassName}`}
+//             onClick={handleClick}
+//         >
+//             <Node content={content} />
+//         </div>
+//     );
+// };
 
-const Node = forwardRef<HTMLDivElement, { content: string }>(({ content }, ref) => {
-    return (
-        <div
-            ref={ref}
-            className="w-[1/4] flex items-center justify-center text-justify text-sm"
-        >
-            <Latex>{content}</Latex>
-        </div>
-    );
-});
+// const Node = forwardRef<HTMLDivElement, { content: string }>(({ content }, ref) => {
+//     return (
+//         <div
+//             ref={ref}
+//             className="w-[1/4] flex items-center justify-center text-justify text-sm"
+//         >
+//             <Latex>{content}</Latex>
+//         </div>
+//     );
+// });

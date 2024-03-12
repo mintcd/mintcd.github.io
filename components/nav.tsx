b5211592a0f6dc7af5647458f1fadfd463d4498f
mintcd <dangminh2208@gmail.com>
2024-03-12 16:37:54 +0700

    Correct the single quote

--- components/nav.tsx
+++ components/nav.tsx
@@ -22,7 +22,7 @@ export default function Nav() {
         />
         <span className="self-center whitespace-nowrap dark:text-white ml-2">
           <div className="text-lg sm:text-2xl font-semibold">In Reseek of Meanings</div>
-          <div className="text-sm sm:text-lg"> What's worth it? How to find? </div>
+          <div className="text-sm sm:text-lg"> What&apos;s worth it? How to find? </div>
         </span>
       </a>
 

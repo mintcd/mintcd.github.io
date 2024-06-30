ed5faf84cc38f2e595f52f56996f3f5f9e418eb6
mintcd <dangminh2208@gmail.com>
2024-06-30 21:53:21 +0700

    complete Lecture View of Knowledge Graph

--- app/mathematics/measure-theory/page.tsx
+++ app/mathematics/measure-theory/page.tsx
@@ -1,21 +1,13 @@
-// import timeline from '@models/mathematics/timelines/measure-theory';
+import timeline from '@models/mathematics/timelines/measure-theory';
 import SubjectMap from '@components/subject-timeline';
 import KnowledgeGraph from '@components/knowledge-graph';
 import graph from '@models/knowledge-graph'
 import { getVerticesOfTopic } from '@functions/graph-analysis';
 
 export default function MeasureTheory() {
-  const measureTheoryGraph = {
-    vertices: getVerticesOfTopic(graph.vertices, ["measure-theory"]),
-    metadata: graph.metadata
-  } as Graph
-
-
   return (
-
     <div className="">
-      {/* <SubjectMap data={timeline} /> */}
-      <KnowledgeGraph graphData={measureTheoryGraph} lectureView={true} />
+      <SubjectMap data={timeline} />
     </div>
   )
 }
--- app/mathematics/real-analysis/page.tsx
+++ app/mathematics/real-analysis/page.tsx
@@ -1,15 +1,11 @@
-// import timeline from '@models/mathematics/timelines/real-analysis';
-// import SubjectMap from '@components/subject-timeline';
-import data from '@models/knowledge-graph'
-import { getVerticesOfTopic } from '@functions/graph-analysis'
-import KnowledgeGraph from '@components/knowledge-graph'
+import timeline from '@models/mathematics/timelines/real-analysis';
+import SubjectMap from '@components/subject-timeline';
+
 
 export default function RealAnalysis() {
-  const realAnalysisData = getVerticesOfTopic(data, ["real-analysis"])
   return (
     <div className={`py-5`}>
-      {/* <SubjectMap data={timeline} /> */}
-      <KnowledgeGraph graph={realAnalysisData} lectureView={true} />
+      <SubjectMap data={timeline} />
     </div>
   )
 }

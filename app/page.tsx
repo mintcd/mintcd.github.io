74857cac7b09237cc24a73aaeb12e7160415df60
Đăng Minh <dangminh2208@gmail.com>
2025-07-27 16:53:16 +0200

    hide homepage

--- app/page.tsx
+++ app/page.tsx
@@ -11,33 +11,34 @@ const NAV_ITEMS = [
 ]
 
 export default function Home() {
-  return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="my-10 sm:my-20 text-center font-extrabold text-5xl sm:text-6xl">
-        Ignite the Meaning?
-      </div>
+  return (<>
+  </>
+    // <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    //   <div className="my-10 sm:my-20 text-center font-extrabold text-5xl sm:text-6xl">
+    //     Ignite the Meaning?
+    //   </div>
 
-      <div className="grid sm:grid-cols-3 gap-6">
-        {NAV_ITEMS.map(({ title, image }, index) => (
-          <Link
-            key={index}
-            href={`/${title.toLowerCase().replace(' ', '-')}`}
-            className="group flex flex-col items-center p-2 rounded-md transition duration-150 ease-in-out w-full hover:opacity-80"
-          >
-            <div className="bg-gray-300 w-full h-11 flex justify-center items-center text-balance">
-              {/* Image soon... */}
-            </div>
-            {/* <Image
-              src={image}
-              alt={title}
-              className="transition-transform duration-300 group-hover:scale-105"
-            /> */}
-            <div className="mt-2 h-20 flex justify-center items-center text-center font-bold text-xl">
-              {title}
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
+    //   <div className="grid sm:grid-cols-3 gap-6">
+    //     {NAV_ITEMS.map(({ title, image }, index) => (
+    //       <Link
+    //         key={index}
+    //         href={`/${title.toLowerCase().replace(' ', '-')}`}
+    //         className="group flex flex-col items-center p-2 rounded-md transition duration-150 ease-in-out w-full hover:opacity-80"
+    //       >
+    //         <div className="bg-gray-300 w-full h-11 flex justify-center items-center text-balance">
+    //           {/* Image soon... */}
+    //         </div>
+    //         {/* <Image
+    //           src={image}
+    //           alt={title}
+    //           className="transition-transform duration-300 group-hover:scale-105"
+    //         /> */}
+    //         <div className="mt-2 h-20 flex justify-center items-center text-center font-bold text-xl">
+    //           {title}
+    //         </div>
+    //       </Link>
+    //     ))}
+    //   </div>
+    // </div>
   )
 }
